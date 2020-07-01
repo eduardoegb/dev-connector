5aebe02ffcde43ce5e75cfad4404d8932905d71e
Eduardo Gonzalez <eduardoegb@gmail.com>
2020-07-01 19:14:45 -0400

    Routes and MongoDB setup

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
 
+const connectDB = require('./config/db');
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('Hello, world'));
+// Connect Database
+connectDB();
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+app.get('/', (req, res) => res.send('API running'));
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
